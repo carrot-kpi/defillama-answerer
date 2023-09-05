17b7b3219df157bd9f69b8f3429b52774cc79094
luzzifoss <fedeluzzi00@gmail.com>
2023-09-05 14:51:18 +0100

    feat: rate limit defillama calls to 10 a seconds max

--- src/defillama.rs
+++ src/defillama.rs
@@ -1,24 +1,37 @@
-use std::str::FromStr;
+use std::{num::NonZeroU32, str::FromStr};
 
 use anyhow::Context;
+use governor::{
+    clock::{QuantaClock, QuantaInstant},
+    middleware::NoOpMiddleware,
+    state::{InMemoryState, NotKeyed},
+    Quota, RateLimiter,
+};
 use reqwest::{Method, Url};
 use rust_decimal::Decimal;
 
 use crate::http_client::HttpClient;
 
+const MAX_CALLS_PER_SECOND: u32 = 10;
+
 // TODO: implement rate limiting
 pub struct DefiLlamaClient {
     http_client: HttpClient,
+    rate_limiter: RateLimiter<NotKeyed, InMemoryState, QuantaClock, NoOpMiddleware<QuantaInstant>>,
 }
 
 impl DefiLlamaClient {
     pub fn new(base_url: Url) -> Self {
         Self {
             http_client: HttpClient::new(base_url),
+            rate_limiter: RateLimiter::direct(Quota::per_second(
+                NonZeroU32::new(MAX_CALLS_PER_SECOND).unwrap(),
+            )),
         }
     }
 
     pub async fn get_current_tvl(&self, protocol: &String) -> anyhow::Result<Decimal> {
+        self.rate_limiter.until_ready().await;
         let raw = self
             .http_client
             .request(Method::GET, format!("/tvl/{protocol}"))?
