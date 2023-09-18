5781d314a5cf293dcb34d45e95129290e161f077
Paolo Guerra <pg.paologuerra@gmail.com>
2023-09-18 17:59:50 +0200

    refactor: move dev mode flag to configuration file

--- src/commons.rs
+++ src/commons.rs
@@ -45,6 +45,7 @@ pub struct Config {
     pub ipfs_api_endpoint: String,
     pub db_connection_string: String,
     pub web3_storage_api_key: Option<String>,
+    pub dev_mode: bool,
     pub api: ApiConfig,
     pub chain_configs: HashMap<u64, ChainConfig>,
 }
